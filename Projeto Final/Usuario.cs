091169080885e3db6abbf7c37eb0603824f2e7c3
GaiaFL <mgcampos99@gmail.com>
2019-10-12 16:43:10 -0300

    classes iniciais

---
+++ Projeto Final/Usuario.cs
@@ -0,0 +1,13 @@
+namespace Projeto_Final
+{
+    public class Usuario
+    {
+        public string token{set;get;}
+        private string senha {set;get;}
+        public string email{set;get;}
+
+        public Usuario(){
+            
+        }
+    }
+}
