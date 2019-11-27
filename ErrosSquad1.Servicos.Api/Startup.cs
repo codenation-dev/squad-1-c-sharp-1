137ef4efdfb0b4fa822cd9433ab22bcaf4fe191a
nadia <nadya.andrushenko@gmail.com>
2019-11-27 19:39:05 -0300

    adicionando token

--- ErrosSquad1.Servicos.Api/Startup.cs
+++ ErrosSquad1.Servicos.Api/Startup.cs
@@ -1,12 +1,15 @@
 ﻿using AutoMapper;
 using ErrosSquad1.Aplicacao;
+using ErrosSquad1.Dominio.Entidades;
 using ErrosSquad1.Infra.Data.Contextos;
 using ErrosSquad1.Infra.IoC;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 
 namespace ErrosSquad1.Servicos.Api
 {
@@ -26,7 +29,27 @@ namespace ErrosSquad1.Servicos.Api
             InjetorDependencias.Registrar(services);
             //services.AddAutoMapper(x => x.AddProfile(new MappingEntidade()));
             services.AddAutoMapper(typeof(MappingEntidade).Assembly);
-            
+            //inicio token
+            var key = System.Text.Encoding.ASCII.GetBytes(Settings.Secret);
+            services.AddCors();
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+            //final token
             services.AddMvc();
         }
 
@@ -36,7 +59,10 @@ namespace ErrosSquad1.Servicos.Api
             {
                 app.UseDeveloperExceptionPage();
             }
-
+           
+            //inicio token
+            app.UseAuthentication();
+            //final token
             app.UseCors(a => a.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
             app.UseMvc();
         }
