fb4495a66e15de50f573c0dae69a80a8a9d008cb
melissatvs <melissa.tv@hotmail.com>
2019-11-29 20:18:29 -0300

    adicionando swagger e correções das buscas de erro

--- ErrosSquad1.Servicos.Api/Startup.cs
+++ ErrosSquad1.Servicos.Api/Startup.cs
@@ -9,8 +9,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
 using Microsoft.IdentityModel.Tokens;
+using Swashbuckle.AspNetCore.Swagger;
 using System;
+using System.IO;
 
 namespace ErrosSquad1.Servicos.Api
 {
@@ -22,7 +25,7 @@ namespace ErrosSquad1.Servicos.Api
         }
 
         public IConfiguration Configuration { get; }
-        
+
         public void ConfigureServices(IServiceCollection services)
         {
             IConfigurationRoot configuration = new ConfigurationBuilder()
@@ -58,6 +61,24 @@ namespace ErrosSquad1.Servicos.Api
 
             #endregion final token
 
+            #region Swagger
+            services.AddSwaggerGen(c =>
+            {
+                string caminhoAplicacao = PlatformServices.Default.Application.ApplicationBasePath;
+                string nomeAplicacao = PlatformServices.Default.Application.ApplicationName;
+                string caminhoDocumentacao = Path.Combine(caminhoAplicacao, $"{ nomeAplicacao}.xml");
+                c.IncludeXmlComments(caminhoDocumentacao);
+
+                c.SwaggerDoc("v1", new Info { Title = "titulo", Version = "v1" });
+
+
+
+            });
+
+
+
+            #endregion
+
             services.AddMvc();
         }
 
@@ -73,7 +94,20 @@ namespace ErrosSquad1.Servicos.Api
             app.UseAuthentication();
             # endregion final token
             app.UseCors(a => a.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+
+
+
             app.UseMvc();
+
+            #region configurações do Swagger
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.RoutePrefix = "swagger";
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                
+            });
+            #endregion
         }
     }
 }
